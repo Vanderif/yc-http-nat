0fd44da02e9a9b746884d4af691d6ef823372796
Ivan Vetkasov <vanderif@yahoo.com>
2024-03-04 16:40:11 +0300

    Using attached_target_group with -parallelism flag instead of GRPC calls

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -25,9 +25,7 @@ resource "yandex_lb_network_load_balancer" "this" {
       subnet_id = length(var.subnet_ids) == 0 ? yandex_vpc_subnet.this[0].id : var.subnet_ids[0]
     }
   }
- 
-  # Those operations become blocked on target group object if it is the same for all balancers. Using GRPC API in local_exec instead.
-  
+
   attached_target_group {
     target_group_id = yandex_lb_target_group.this.id
 
@@ -42,12 +40,4 @@ resource "yandex_lb_network_load_balancer" "this" {
       }
     }
   }
-  
-/*
-  provisioner "local-exec" {
-    command = <<-CMD
-    sleep ${count.index * 30} && curl -s -d '{ attachedTargetGroup: { targetGroupId: "${yandex_lb_target_group.this.id}", healthChecks: [{ name: "custom", interval: "30s", timeout: "10s", unhealthyThreshold: 2, healthyThreshold: 2, tcpOptions: {port: ${var.yc_endpoints_struct[count.index].port}} }] }}' -H "Authorization: Bearer $YC_TOKEN" -X POST https://load-balancer.api.cloud.yandex.net/load-balancer/v1/networkLoadBalancers/${self.id}:attachTargetGroup
-    CMD
-  }
-*/
 }
